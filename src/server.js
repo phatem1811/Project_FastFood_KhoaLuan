bead52c4e5f5da9be2d4b77f3661fd388bfc70e8
DESKTOP-MA85DGD\DELL <huynhtienphat1811@gmail.com>
2024-10-23 20:33:04 +0700

    ingredient

--- src/server.js
+++ src/server.js
@@ -22,19 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// const accountSid = 'AC6bf7fc1378d2495e95e52ae61b32d397';
-// const authToken = '22bc7dff11cad29aaa55f2c39a73bd24';
-
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//     body: 'Hello from twilio-node',
-//     to: '+15625536533', // Text your number
-//     from: '+15177265041', // From a valid Twilio number
-//   })
-//   .then((message) => console.log(message.sid));
-
 app.use("/v1",APIs );
 
 
